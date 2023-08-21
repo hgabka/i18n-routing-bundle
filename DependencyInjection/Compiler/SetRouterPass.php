08b4711ed8640faa9da1de27d9cdd00f3f777a9c
hgabka <hgabka@gmail.com>
2023-08-21 12:34:10 +0200

    Update SetRouterPass.php

--- DependencyInjection/Compiler/SetRouterPass.php
+++ DependencyInjection/Compiler/SetRouterPass.php
@@ -13,7 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class SetRouterPass implements CompilerPassInterface
 {
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
         $container->setAlias('router', 'jms_i18n_routing.router')->setPublic(true);
 
